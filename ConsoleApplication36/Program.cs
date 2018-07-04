125575f2b98e9892f42c19885415c7187c558d37
Chirag <Chirag@CHIRAG-PC>
2018-07-03 19:28:20 -0500

    Add project files.

---
+++ ConsoleApplication36/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 153;
+
+            string valuestringcommas = "";
+            double sum = 0;
+            string valuestring = value.ToString();
+            for (int i = 0; i < valuestring.Length; i++)
+            {
+                valuestringcommas = valuestringcommas + valuestring[i] + ",";
+            }
+            valuestringcommas = valuestringcommas.Remove(valuestringcommas.Length - 1);
+            double[] numberArray = valuestringcommas.Split(',').Select(double.Parse).ToArray();
+
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                double converted = numberArray[i];
+                double exponent = Math.Pow(converted, numberArray.Length);
+                sum = sum + exponent;
+            }
+
+            int finalanswer = (int)sum;
+
+            if (finalanswer ==value)
+            {
+                Console.WriteLine("true");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("false");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
